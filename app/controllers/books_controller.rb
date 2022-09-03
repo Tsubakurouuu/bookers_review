eed4c5b76aa66e88dcb01a66a0b4c269b760dbf7
Tsubakurouuu <yzpolice2896@icloud.com>
2022-09-03 23:30:00 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,17 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
     @book = Book.new(book_params)
     # binding.pry
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -18,23 +23,28 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
+    flash[:notice] = "Book was successfully destroyed."
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
